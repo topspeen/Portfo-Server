8a0698351dc36095d882b0ed0a95addafc5a592b
topspeen <doberman.info@gmail.com>
2022-11-21 14:42:44 +0200

    clean

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var createError = require('http-errors');
 var express = require('express');
-var axios = require('axios');
 let dataFromCMC;
 let metaDataFromCMC;
 let dataFromCoinGecko;
@@ -30,78 +29,6 @@ var corsOptions = {
 }
 
 app.use(cors(corsOptions));
-
-
-
-
-// fetch data from CMC;
-// var data_config = {
-//     method: 'get',
-//     url: 'https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?id=1,2,74,1027,6636,1839',
-//     headers: {
-//         'X-CMC_PRO_API_KEY': '69e3b39b-71c3-4c16-bf79-12a42a042a25',
-//         'Accept': 'application/json',
-//         'Accept-Encoding': 'deflate, gzip'
-//     }
-// };
-// fetch data from Coingecko;
-// var dataFromCoinGecko_config = {
-//     method: 'get',
-//     url: 'https: //api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=7d',
-//     headers: {
-//         'Accept': 'application/json',
-//         'Accept-Encoding': 'deflate, gzip'
-//     }
-// };
-
-// axios(dataFromCoinGecko_config)
-//     .then(function(response) {
-//         dataFromCoinGecko = response.data;
-//     }).then(function(response) {
-//         // console.log(JSON.stringify(dataFromCMC));
-//     }).catch(function(error) {
-//         console.log(error);
-//     }).then(function() {
-
-//     });
-
-// fetch metaData from CMC;
-// var metaData_config = {
-//     method: 'get',
-//     url: 'https://pro-api.coinmarketcap.com/v2/cryptocurrency/info?id=1,2,74,1027,6636,1839',
-//     headers: {
-//         'X-CMC_PRO_API_KEY': '69e3b39b-71c3-4c16-bf79-12a42a042a25',
-//         'Accept': 'application/json',
-//         'Accept-Encoding': 'deflate, gzip'
-//     }
-// };
-
-
-
-// setInterval(function() {
-// axios(metaData_config)
-//     .then(function(response) {
-//         metaDataFromCMC = response.data;
-//     }).then(function(response) {
-//         // console.log(JSON.stringify(metaDataFromCMC.data[2]));
-//     }).catch(function(error) {
-//         console.log(error);
-//     }).then(function() {
-
-//     });
-
-// axios(data_config)
-//     .then(function(response) {
-//         dataFromCMC = response.data;
-//     }).then(function(response) {
-//         // console.log(JSON.stringify(dataFromCMC));
-//     }).catch(function(error) {
-//         console.log(error);
-//     }).then(function() {
-
-//     });
-
-// }, 60000);
 
 // send data to ReactApp
 app.get('/', (req, res) => {
